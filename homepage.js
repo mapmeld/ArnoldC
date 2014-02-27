fff1da11489bd46ef66be6b3c9138078fb3d4bb5
Nick Doiron <ndoiron@mapmeld.com>
2014-02-27 12:40:04 -0600

    Load sample program

--- homepage.js
+++ homepage.js
@@ -4,6 +4,11 @@ var myCodeMirror = CodeMirror( $("#program")[0], {
 });
 
 myCodeMirror.setValue("IT'S SHOWTIME\nYOU HAVE BEEN TERMINATED");
+function loadProgram(){
+  // sample counter
+  myCodeMirror.setValue("IT'S SHOWTIME\n  HEY CHRISTMAS TREE isLessThan10\n    YOU SET US UP @NO PROBLEMO\n  HEY CHRISTMAS TREE n\n    YOU SET US UP 0\n  STICK AROUND isLessThan10\n    GET TO THE CHOPPER n\n      HERE IS MY INVITATION n\n      GET UP 1\n    ENOUGH TALK\n    TALK TO THE HAND n\n    GET TO THE CHOPPER isLessThan10\n      HERE IS MY INVITATION 10\n      LET OFF SOME STEAM BENNET n\n    ENOUGH TALK\n  CHILL\nYOU HAVE BEEN TERMINATED");
+  execute_code($(".CodeMirror"));  
+}
 
 var lastcode = myCodeMirror.getValue();
 var refreshTimeout = null;
